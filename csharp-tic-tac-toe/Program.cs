b3c104c3db630a89705bc435a9eff4a8c174e0ab
Luqman <luqdek@gmail.com>
2021-07-01 18:44:34 +0100

    initialise grid with empty chars

--- csharp-tic-tac-toe/Program.cs
+++ csharp-tic-tac-toe/Program.cs
@@ -5,6 +5,17 @@ namespace csharp_tic_tac_toe
     class Board
     {
         private char[,] Grid = new char[3, 3];
+
+        public Board()
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Grid[i,j] = ' ';
+                }
+            }
+        }
         public void Draw()
         {
             Console.WriteLine("TEst");
