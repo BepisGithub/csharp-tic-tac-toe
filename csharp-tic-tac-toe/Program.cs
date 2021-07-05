c7676c4ceebf2a113291c29bde93ca149d073800
Luqman <luqdek@gmail.com>
2021-07-05 05:51:03 +0100

    fix syntax error with assigning active player

--- csharp-tic-tac-toe/Program.cs
+++ csharp-tic-tac-toe/Program.cs
@@ -215,14 +215,15 @@ namespace csharp_tic_tac_toe
 
         static Player Round(Player P1, Player P2)
         {
+            Player Active;
             //Determine active player
             if (P1.Active)
             {
-                Player Active = P1;
+                Active = P1;
             }
             else
             {
-                Player Active = P2;
+                Active = P2;
             }
             return null;
         }
